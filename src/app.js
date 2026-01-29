e180ae2b2d03339c33fde31a18949132ae768ef7
VictoriaIngrid <victoriaingrid685@gmail.com>
2026-01-28 22:20:23 -0300

    Adiciona CORS para permitir acesso do frontend

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
+import cors from "cors";
 import express from "express";
 import tarefasRouter from "./routes/tarefasRoutes.js";
 import { requestLogger } from "./middlewares/logger.js";
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
 
 app.use(requestLogger);
-app.use(express.json());
 
 // rotas
 app.use("/tarefas", tarefasRouter);
